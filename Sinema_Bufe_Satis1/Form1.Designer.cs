24dbbfbb6d02e1fb9273ef088096063c1a4d56f3
Kubilay BAYINDIR <45577585+bayindirkubilay60@users.noreply.github.com>
2023-03-30 19:04:58 +0300

    Add project files.

---
+++ Sinema_Bufe_Satis1/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace Sinema_Bufe_Satis1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TicketTbx = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TeaTbx = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.WaterTbx = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CornTbx = new System.Windows.Forms.TextBox();
+            this.Mısırlbl = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SumBtn = new System.Windows.Forms.Button();
+            this.AllTotalPriceLbl = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.TotalPriceLbl = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Controls.Add(this.TicketTbx);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.TeaTbx);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.WaterTbx);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.CornTbx);
+            this.groupBox1.Controls.Add(this.Mısırlbl);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 181);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product Quantity";
+            // 
+            // TicketTbx
+            // 
+            this.TicketTbx.Location = new System.Drawing.Point(120, 130);
+            this.TicketTbx.Name = "TicketTbx";
+            this.TicketTbx.Size = new System.Drawing.Size(100, 26);
+            this.TicketTbx.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(35, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ticket";
+            // 
+            // TeaTbx
+            // 
+            this.TeaTbx.Location = new System.Drawing.Point(120, 104);
+            this.TeaTbx.Name = "TeaTbx";
+            this.TeaTbx.Size = new System.Drawing.Size(100, 26);
+            this.TeaTbx.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(35, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tea";
+            // 
+            // WaterTbx
+            // 
+            this.WaterTbx.Location = new System.Drawing.Point(120, 78);
+            this.WaterTbx.Name = "WaterTbx";
+            this.WaterTbx.Size = new System.Drawing.Size(100, 26);
+            this.WaterTbx.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(35, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Water";
+            // 
+            // CornTbx
+            // 
+            this.CornTbx.Location = new System.Drawing.Point(120, 52);
+            this.CornTbx.Name = "CornTbx";
+            this.CornTbx.Size = new System.Drawing.Size(100, 26);
+            this.CornTbx.TabIndex = 1;
+            // 
+            // Mısırlbl
+            // 
+            this.Mısırlbl.AutoSize = true;
+            this.Mısırlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Mısırlbl.Location = new System.Drawing.Point(35, 52);
+            this.Mısırlbl.Name = "Mısırlbl";
+            this.Mısırlbl.Size = new System.Drawing.Size(43, 20);
+            this.Mısırlbl.TabIndex = 0;
+            this.Mısırlbl.Text = "Corn";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(269, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(141, 181);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Price Table";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ticket: 8 $";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(33, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tea:     2 $";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(33, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Water: 1 $";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(33, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Corn:   4 $";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox3.Controls.Add(this.ClearBtn);
+            this.groupBox3.Controls.Add(this.SumBtn);
+            this.groupBox3.Controls.Add(this.AllTotalPriceLbl);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.TotalPriceLbl);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(416, 132);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(234, 181);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Cash";
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.ClearBtn.Location = new System.Drawing.Point(23, 128);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(183, 31);
+            this.ClearBtn.TabIndex = 12;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // SumBtn
+            // 
+            this.SumBtn.BackColor = System.Drawing.Color.LimeGreen;
+            this.SumBtn.Location = new System.Drawing.Point(23, 51);
+            this.SumBtn.Name = "SumBtn";
+            this.SumBtn.Size = new System.Drawing.Size(183, 31);
+            this.SumBtn.TabIndex = 11;
+            this.SumBtn.Text = "Sum";
+            this.SumBtn.UseVisualStyleBackColor = false;
+            this.SumBtn.Click += new System.EventHandler(this.HesaplaBtn_Click);
+            // 
+            // AllTotalPriceLbl
+            // 
+            this.AllTotalPriceLbl.AutoSize = true;
+            this.AllTotalPriceLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.AllTotalPriceLbl.Location = new System.Drawing.Point(153, 99);
+            this.AllTotalPriceLbl.Name = "AllTotalPriceLbl";
+            this.AllTotalPriceLbl.Size = new System.Drawing.Size(44, 20);
+            this.AllTotalPriceLbl.TabIndex = 9;
+            this.AllTotalPriceLbl.Text = "0,0 $";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.Location = new System.Drawing.Point(19, 99);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(108, 20);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "All Total Price:";
+            // 
+            // TotalPriceLbl
+            // 
+            this.TotalPriceLbl.AutoSize = true;
+            this.TotalPriceLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TotalPriceLbl.Location = new System.Drawing.Point(153, 23);
+            this.TotalPriceLbl.Name = "TotalPriceLbl";
+            this.TotalPriceLbl.Size = new System.Drawing.Size(44, 20);
+            this.TotalPriceLbl.TabIndex = 6;
+            this.TotalPriceLbl.Text = "0,0 $";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(19, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(87, 20);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Total Price:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(214, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(241, 22);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Simple Cinema Order Screen";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(453, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 115);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 114);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(660, 342);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Sales Screen";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TicketTbx;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TeaTbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox WaterTbx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CornTbx;
+        private System.Windows.Forms.Label Mısırlbl;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label TotalPriceLbl;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label AllTotalPriceLbl;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button SumBtn;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
---
+++ Sinema_Bufe_Satis1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinema_Bufe_Satis1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int TotalPrice = 0;
+        int AllTotalPrice = 0;
+        int corn, water, tea, ticket;
+
+        private void HesaplaBtn_Click(object sender, EventArgs e)
+        {
+
+            corn = Convert.ToInt16(CornTbx.Text == null || CornTbx.Text == "" ? "0" : CornTbx.Text);
+            water = Convert.ToInt16(WaterTbx.Text == null || WaterTbx.Text == "" ? "0" : WaterTbx.Text);
+            tea = Convert.ToInt16(TeaTbx.Text == null || TeaTbx.Text == "" ? "0" : TeaTbx.Text);
+            ticket = Convert.ToInt16(TicketTbx.Text == null || TicketTbx.Text == "" ? "0" : TicketTbx.Text);
+
+            TotalPrice = corn * 4 + water * 1 + tea * 2 + ticket * 8;
+            AllTotalPrice += TotalPrice;
+
+            TotalPriceLbl.Text = TotalPrice.ToString() + " $";
+            AllTotalPriceLbl.Text = AllTotalPrice.ToString() + " $";
+
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            // Variable
+            string MessageBoxTitle = "Delete";
+            string MessageBoxContent = "The data will be deleted. Are you sure? ";
+
+            DialogResult dialogResult = MessageBox.Show(MessageBoxContent, MessageBoxTitle, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                corn = 0;
+                water = 0;
+                tea = 0;
+                ticket = 0;
+                TotalPrice = 0;
+                AllTotalPrice = 0;
+                CornTbx.Text = "";
+                WaterTbx.Text = "";
+                TeaTbx.Text = "";
+                TicketTbx.Text = "";
+                TotalPriceLbl.Text = "0,0 TL";
+                AllTotalPriceLbl.Text = "0,0 TL";
+
+                CornTbx.Focus();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+    }
+}
+
